bdec13c3907c3110610597c469445595bfefb37f
Chandan-Deb <chandan457deb@gmail.com>
2024-08-18 14:37:44 +0530

    testForServer

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, '../front-end')));
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'mysql',
+    password: 'helloworld',
     database: 'legends_db'
 });
 
